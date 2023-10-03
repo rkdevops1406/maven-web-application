05144ed5518f328e5749c4c7d7893052de042826
rkdevops1406 <139582183+rkdevops1406@users.noreply.github.com>
2023-10-03 10:32:58 -0400

    Update raju.groovy

--- raju.groovy
+++ raju.groovy
@@ -4,7 +4,7 @@ pipeline{
         stage("code"){
             steps{
             //    git branch: 'main', credentialsId: 'gitcred', url: 'https://github.com/rkdevops1406/maven-web-application.git'
-               git credentialsId: 'ceadcc92-006e-4df4-a5a6-c552f1dc2e1a', url: 'https://github.com/rkdevops1406/maven-web-application-old.git'
+               git credentialsId: 'gitcred', url: 'https://github.com/rkdevops1406/maven-web-application.git'
             }
         }
         stage("Build"){
@@ -16,8 +16,8 @@ pipeline{
         }
         stage("Deploy to tomcat"){
             steps{
-                sshagent(['a8ae6ec2-5567-425d-a1ee-442cd325710b']) {
-                sh 'scp -o StrictHostKeyChecking=no /var/lib/jenkins/workspace/Pipeline-1/target/maven-web-application.war  ec2-user@18.222.11.106:/opt/apache-tomcat-9.0.80/webapps'   
+                sshagent(['70844032-23e8-4e8d-82ca-43e2ce6a40fb']) {
+                sh 'scp -o StrictHostKeyChecking=no /var/lib/jenkins/workspace/Pipeline-1/target/maven-web-application.war  ec2-user@3.145.199.169:/opt/apache-tomcat-9.0.80/webapps'   
     
                 }
             }
